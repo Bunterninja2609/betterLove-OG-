ae033ec33895ee3c88d74c9df06fccf0bd0afb56
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2024-01-15 12:47:21 +0100

    Update main.lua
    Test fil

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
-
+--test file for betterMath and love3d
+betterMath = require(BetterMath)
+love3d = require(love3d)
+function love.load()
+end
+function love.update(dt)
+end
+function love.draw()
+end
